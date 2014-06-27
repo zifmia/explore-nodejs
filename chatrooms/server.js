beffd3a40965493d9d78af43611fe061ea56715b
James Layland <James.Layland@perceptivesoftware.com>
2014-06-27 16:33:58 -0500

    adding chat server

--- chatrooms/server.js
+++ chatrooms/server.js
@@ -2,6 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
+var chatServer = require('./lib/chat_server');
+
 var cache = {};
 
 var send404 = function (response) {
@@ -62,4 +64,5 @@ server.listen(3000, function () {
     console.log('Server listening on port 3000.');
 });
 
+chatServer.listen(server);
 
